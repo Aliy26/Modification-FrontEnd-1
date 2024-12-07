a8c2f3b76f237a93acb733478b1a7e45f133a1df
Umar_Aliy <umaraliy092@gmail.com>
2024-12-08 06:05:39 +0900

    fixed deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ npm i yarn -g
 yarn global add serve
 yarn
 yarn run build 
-pm2 start yarn --name GATORADE-REACT --interpreter cmd -- start:prod
+pm2 start "yarn run start:prod" --name=GATORADE-REACT
